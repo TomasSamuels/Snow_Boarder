b7551aa0c0a6a3595a86b1a0fd956f872b46d6de
Tomas Samuels <tomassamuelsb@gmail.com>
2025-04-20 15:23:54 -0600

    Movement, Crash and finish line added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float torqueAmount = 2f;
+    Rigidbody2D rb2d;
+    void Start()
+    {
+      rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+      if(Input.GetKey(KeyCode.LeftArrow))
+      {
+        rb2d.AddTorque(torqueAmount);
+      }
+      else if(Input.GetKey(KeyCode.RightArrow))
+      {
+        rb2d.AddTorque(-torqueAmount);
+      }
+    }
+}
